9dd22061cccd4be7ee06066c40854ad2b595d677
RockyKing <rockymars8@gmail.com>
2023-01-06 09:09:49 -0400

    Viernes 623
    creamos una clase manejovar para probar valores boleanos

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, Indie World!");
+        System.out.println("Hello, Indie World! iam the constructor matrix");
     }
 }
---
+++ src/ManejoVar.java
@@ -0,0 +1,13 @@
+public class ManejoVar {
+    public static void main(String args[]) {
+        // variables bolean, declaration
+        boolean bool1;
+        // inicializacion
+        bool1 = true;
+        // declaracion e inicializacion
+        boolean bool2 = false;
+        System.out.println("el valor de bool1 es: " + bool1);
+        System.out.println("el valor de bool2 es: " + bool2);
+        System.out.println("");
+    }
+}
